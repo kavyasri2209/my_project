cbf74c5fad6b82641d677c5f5842276981253b16
kavyasri2209 <kavyasri336@gmail.com>
2025-10-04 20:29:43 +0530

    hash routes updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+
 import Home from './components/Home';
 import Intro from './components/Intro';
 import About from './components/About';
@@ -11,35 +13,51 @@ import Grades from './components/Grades';
 import Projects from './components/Projects';
 import Form from './components/Form';
 import Cards from './components/Cards';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Intro />
-      <About />
-      <Skills />
-      <Grades />
-      <Projects />
-      <Form />
-      <Cards />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Footer />
-    </div>
-    
+    <Router>
+      <div className="App">
+
+        {/* Navbar */}
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+          <div className="container-fluid container">
+            <a className="navbar-brand fw-bold" href="/">My Portfolio</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
+                <li className="nav-item"><a className="nav-link" href="/intro">Intro</a></li>
+                <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
+                <li className="nav-item"><a className="nav-link" href="/skills">Skills</a></li>
+                <li className="nav-item"><a className="nav-link" href="/grades">Grades</a></li>
+                <li className="nav-item"><a className="nav-link" href="/projects">Projects</a></li>
+                <li className="nav-item"><a className="nav-link" href="/form">Contact</a></li>
+                <li className="nav-item"><a className="nav-link" href="/cards">Cards</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/intro" element={<Intro />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/grades" element={<Grades />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/cards" element={<Cards />} />
+        </Routes>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,13 @@
-import React from 'react'
+import React from 'react';
+// optional, if your Home content is split
 
 function Home() {
   return (
-    <div>
-          <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <div class="container-fluid container">
-            <a class="navbar-brand fw-bold" href="#">My Portfolio</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#home">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#about">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#skills">Skills</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <div id="home" style={{ paddingTop: "80px" }}>
+      <h1 className="text-center">Welcome to My Portfolio</h1>
+      <p className="text-center">This is the homepage content.</p>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
